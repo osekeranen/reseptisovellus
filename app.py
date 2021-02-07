5090bc0d6429206208c9762a3e45ca56238363c5
Oskari Keränen <oskari.e.keranen@gmail.com>
2021-02-08 01:01:08 +0200

    Add initial app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    result = db.session.execute("SELECT COUNT(*) FROM recipes")
+    count = result.fetchone()[0]
+    return render_template("index.html", count=count)
