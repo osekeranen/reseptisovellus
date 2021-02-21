ba3ff5abcf3e40681a478b5e9acadb7b6c81aefa
Oskari Keränen <oskari.e.keranen@gmail.com>
2021-02-21 16:26:04 +0200

    Add template for adding new ingredients

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 from db import db
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def index():
@@ -11,7 +11,7 @@ def index():
     return render_template("index.html", count=count, recipes=recipes)
 
 @app.route("/resepti/<int:id>")
-def page(id):
+def recipe(id):
     result = db.session.execute("SELECT name FROM recipes WHERE id="+str(id))
     name = result.fetchone()[0]
     result = db.session.execute("SELECT recipes.id, recipes_steps.step, recipes_steps.description, steps_ingredients.amount, measurements.abbreviation, ingredients.partitive FROM recipes LEFT JOIN recipes_steps ON recipes.id = recipes_steps.recipe_id LEFT JOIN steps_ingredients ON recipes_steps.id = steps_ingredients.step_id LEFT JOIN measurements ON steps_ingredients.measurement_id = measurements.id LEFT JOIN ingredients ON steps_ingredients.ingredient_id = ingredients.id WHERE recipes.id="+str(id))
@@ -20,9 +20,17 @@ def page(id):
     steps = result.fetchall()
     return render_template("recipe.html", name=name, ingredients=ingredients, steps=steps)
 
-@app.route("/uusi")
+@app.route("/uusi/resepti")
 def new_recipe():
     return render_template("new_recipe.html")
+
+@app.route("/uusi/ainesosa")
+def new_ingredient():
+    return render_template("new_ingredient.html")
+
+@app.route("/uusi/ainesosa/lisatty", methods=["POST"])
+def new_ingredient_result():
+    return render_template("ingredient_result.html", ingredient=request.form["ingredient"], partitive=request.form["partitive"])
 
 @app.route("/mitat.csv")
 def get_measurements():
